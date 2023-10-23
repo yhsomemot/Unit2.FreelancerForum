5df46a992f530fa1f4b579ff1b2cce5c5adb546d
Young <youngheesomemoto@gmail.com>
2023-10-22 19:47:12 -0500

    block19

---
+++ index.js
@@ -0,0 +1,10 @@
+// have listing appear? have listing appear after clicking to another page. 
+// (new page everytime: The home page is a "new page")
+//observe list of Alice; $30
+//go home
+//see bob, teacher, starting price $50
+//go home
+//see carol, programmer starign price 70
+//average staring price is updated to 50
+
+//new freelancers appear every few seconds, and the starting price is updated accordingly. 
